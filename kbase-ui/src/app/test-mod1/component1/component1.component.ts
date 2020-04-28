b5963b1c2f105365c49c2bcc47b3cdd78c57709c
robertsayfullin <robert.sayfullin@gmail.com>
2020-04-28 19:19:02 +1000

    deep triple copy before manipulation formatting

--- kbase-ui/src/app/test-mod1/component1/component1.component.ts
+++ kbase-ui/src/app/test-mod1/component1/component1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 declare var $rdf: any;
 declare var d3: any;
@@ -10,63 +10,63 @@ declare var d3: any;
 })
 export class Component1Component implements OnInit {
 
-  store = $rdf.graph();
-  fetcher = new $rdf.Fetcher(this.store, 5000);
-  ttl_urls= [
+  store = null;
+  fetcher = null;
+  ttl_urls = [
     "http://localhost:4200/assets/ttl/people/",
     "http://localhost:4200/assets/ttl/data_catalog/"
   ];
 
   ttls = [["llama.ttl", "marilena.ttl", "marta.ttl", "people.ttl", "relationship.ttl", "robert.ttl", "maya.ttl", "sample.ttl"],
     [
-    "data_set/chronos_data_lake.ttl",
+      "data_set/chronos_data_lake.ttl",
 
-    "data_set/fls/fls_schema_v0.ttl",
-    "data_set/fls/fls_thor.ttl",
-    "data_set/fls/fls_thor_s3_distribution.ttl",
+      "data_set/fls/fls_schema_v0.ttl",
+      "data_set/fls/fls_thor.ttl",
+      "data_set/fls/fls_thor_s3_distribution.ttl",
 
-    "data_set/fls_sharp/fls_sharp.ttl",
-    "data_set/fls_sharp/fls_sharp_s3_distribution.ttl",
-    "data_set/fls_sharp/fls_sharp_schema_v0.ttl",
+      "data_set/fls_sharp/fls_sharp.ttl",
+      "data_set/fls_sharp/fls_sharp_s3_distribution.ttl",
+      "data_set/fls_sharp/fls_sharp_schema_v0.ttl",
 
-    "data_set/gtas/gtas.ttl",
-    "data_set/gtas/gtas_es_distributions.ttl",
-    "data_set/gtas/lqd_schema_v0.ttl",
-    "data_set/gtas/mlt_schema_v0.ttl",
-    "data_set/gtas/selt_schema_v0.ttl",
-    "data_set/gtas/testDetails.ttl",
-    "data_set/gtas/testSummary.ttl",
+      "data_set/gtas/gtas.ttl",
+      "data_set/gtas/gtas_es_distributions.ttl",
+      "data_set/gtas/lqd_schema_v0.ttl",
+      "data_set/gtas/mlt_schema_v0.ttl",
+      "data_set/gtas/selt_schema_v0.ttl",
+      "data_set/gtas/testDetails.ttl",
+      "data_set/gtas/testSummary.ttl",
 
-    "data_set/hlog/hlog.ttl",
-    "data_set/hlog/hlog_s3_distribution.ttl",
-    "data_set/hlog/hlog_schema_v0.ttl",
+      "data_set/hlog/hlog.ttl",
+      "data_set/hlog/hlog_s3_distribution.ttl",
+      "data_set/hlog/hlog_schema_v0.ttl",
 
-    "data_set/nac/nac_ports.ttl",
-    "data_set/nac/nac_ports_s3_distribution.ttl",
-    "data_set/nac/nac_ports_schema_v0.ttl",
-    "data_set/nac/nac_ports_schema_v1.ttl",
+      "data_set/nac/nac_ports.ttl",
+      "data_set/nac/nac_ports_s3_distribution.ttl",
+      "data_set/nac/nac_ports_schema_v0.ttl",
+      "data_set/nac/nac_ports_schema_v1.ttl",
 
-    "data_set/sdc/sdc.ttl",
-    "data_set/sdc/sdcEsCache_distribution.ttl",
-    "data_set/sdc/sdcS3_distribution.ttl",
-    "data_set/sdc/sdc_schema_v0.ttl",
+      "data_set/sdc/sdc.ttl",
+      "data_set/sdc/sdcEsCache_distribution.ttl",
+      "data_set/sdc/sdcS3_distribution.ttl",
+      "data_set/sdc/sdc_schema_v0.ttl",
 
-    "data_set/services/athena_proxy_service.ttl",
-    "data_set/services/es_proxy_service.ttl",
+      "data_set/services/athena_proxy_service.ttl",
+      "data_set/services/es_proxy_service.ttl",
 
-    // "dq/HLOGs3_correctness_dq_queries.ttl",
-    // "dq/NAC_correctness_DQ_Queries.ttl",
-    // "dq/NACs3_completeness_dq_queries.ttl",
-    // "dq/SDCs3_completeness_dq_queries.ttl",
-    // "dq/sdcS3_correctness_dq_queries.ttl",
-    // "dq/sdcS3_distribution_dq.ttl",
+      // "dq/HLOGs3_correctness_dq_queries.ttl",
+      // "dq/NAC_correctness_DQ_Queries.ttl",
+      // "dq/NACs3_completeness_dq_queries.ttl",
+      // "dq/SDCs3_completeness_dq_queries.ttl",
+      // "dq/sdcS3_correctness_dq_queries.ttl",
+      // "dq/sdcS3_distribution_dq.ttl",
 
-    "services/athena_proxy_service.ttl",
-    "services/es_proxy_service.ttl"
-  ]
+      "services/athena_proxy_service.ttl",
+      "services/es_proxy_service.ttl"
+    ]
   ];
 
-  starting_points=["http://example/knowledge-base/marilena","http://chronos/data-lake/chronosDataLake"];
+  starting_points = ["http://example/knowledge-base/marilena", "http://chronos/data-lake/chronosDataLake"];
 
   predicate_sort_order = [
     //people
@@ -118,21 +118,21 @@ export class Component1Component implements OnInit {
 
   }
 
-  load_ttl(setNdx){
+  load_ttl(setNdx) {
     this.store = $rdf.graph();
-    this.fetcher = new $rdf.Fetcher(this.store, 5000);
+    this.fetcher = new $rdf.Fetcher(this.store, 1000);
 
     console.log("Loading turtles");
-    this.ttls[setNdx].forEach((ttl)=>{
+    this.ttls[setNdx].forEach((ttl) => {
       console.log(ttl);
-      this.fetcher.nowOrWhenFetched(this.ttl_urls[setNdx] + ttl, function(isOk, body, xhr) {
+      this.fetcher.nowOrWhenFetched(this.ttl_urls[setNdx] + ttl, function (isOk, body, xhr) {
         if (!isOk) {
           alert("Oops, something happened and couldn't fetch data");
         }
       });
     });
 
-    setTimeout(()=>{
+    setTimeout(() => {
       this.onNodeClick(this.store.sym(this.starting_points[setNdx]));
     }, 1000);
   }
@@ -146,119 +146,133 @@ export class Component1Component implements OnInit {
     this.triples = this.store.match();
   }
 
-    // termType == "NameNode", "Literal", "BlankNode"
+  // termType == "NameNode", "Literal", "BlankNode"
 
   addTriplesIntoNet(combined_triples, triples_to_add) {
-    triples_to_add.forEach((triple)=>{
-      let existing_triple_for_same_subj_obj = combined_triples.find(existing_triple=>
+    triples_to_add.forEach((triple) => {
+      let existing_triple_for_same_subj_obj = combined_triples.find(existing_triple =>
         triple.subject.value == existing_triple.subject.value &&
         triple.object.value == existing_triple.object.value
       );
-      if (existing_triple_for_same_subj_obj){
-        existing_triple_for_same_subj_obj.links.push({predicate:triple.predicate, direction:"in"});
+      if (existing_triple_for_same_subj_obj) {
+        existing_triple_for_same_subj_obj.links.push({predicate: triple.predicate, direction: "in"});
       }
-      let existing_reverse_triple_for_same_subj_obj = combined_triples.find(existing_triple=>
+      let existing_reverse_triple_for_same_subj_obj = combined_triples.find(existing_triple =>
         triple.object.value == existing_triple.subject.value &&
         triple.subject.value == existing_triple.object.value
       );
-      if (existing_reverse_triple_for_same_subj_obj){
-        existing_reverse_triple_for_same_subj_obj.links.push({predicate:triple.predicate, direction:"out"});
+      if (existing_reverse_triple_for_same_subj_obj) {
+        existing_reverse_triple_for_same_subj_obj.links.push({predicate: triple.predicate, direction: "out"});
       }
-      if (!existing_triple_for_same_subj_obj && !existing_reverse_triple_for_same_subj_obj){
-        combined_triples.push({subject:triple.subject, links:[{predicate:triple.predicate, direction:"in"}], object:triple.object});
+      if (!existing_triple_for_same_subj_obj && !existing_reverse_triple_for_same_subj_obj) {
+        combined_triples.push({
+          subject: triple.subject,
+          links: [{predicate: triple.predicate, direction: "in"}],
+          object: triple.object
+        });
       }
     });
     return combined_triples;
   }
 
   filterTripleWithLiteralObject = (triple) => triple.object && triple.object.termType === "Literal";
-  filterNavigableNodes = (triple) => triple.object &&  (triple.object.termType === "NamedNode" || triple.object.termType === "BlankNode");
+  filterNavigableNodes = (triple) => triple.object && (triple.object.termType === "NamedNode" || triple.object.termType === "BlankNode");
 
   reducerGroupByPredicate = (accumulator, currentTriple, currentIndex, array) => {
-    let existingPredicateNdx = accumulator.findIndex((triple)=>triple.predicate.value==currentTriple.predicate.value);
-    if (existingPredicateNdx >= 0){
-      accumulator[existingPredicateNdx].object += (", "+ currentTriple.object);
-    }else{
+    let existingPredicateNdx = accumulator.findIndex((triple) => triple.predicate.value == currentTriple.predicate.value);
+    if (existingPredicateNdx >= 0) {
+      accumulator[existingPredicateNdx].object += (", " + currentTriple.object);
+    } else {
       accumulator.push(currentTriple);
     }
     return accumulator;
   };
   reducerGroupByPredicateForLinkableObjects = (accumulator, currentTriple, currentIndex, array) => {
-    let existingPredicateNdx = accumulator.findIndex((triple)=>triple.predicate.value==currentTriple.predicate.value);
-    if (existingPredicateNdx >= 0){
-      if (!accumulator[existingPredicateNdx].objects){
+    let existingPredicateNdx = accumulator.findIndex((triple) => triple.predicate.value == currentTriple.predicate.value);
+    if (existingPredicateNdx >= 0) {
+      if (!accumulator[existingPredicateNdx].objects) {
         accumulator[existingPredicateNdx].objects = [accumulator[existingPredicateNdx].object];
         accumulator[existingPredicateNdx].object = null;
       }
       accumulator[existingPredicateNdx].objects.push(currentTriple.object);
-    }else{
+    } else {
       accumulator.push(currentTriple);
     }
     return accumulator;
   };
 
   reducerGroupByPredicateForLinkableSubjects = (accumulator, currentTriple, currentIndex, array) => {
-    let existingPredicateNdx = accumulator.findIndex((triple)=>triple.predicate.value==currentTriple.predicate.value);
-    if (existingPredicateNdx >= 0){
-      if (!accumulator[existingPredicateNdx].subjects){
+    let existingPredicateNdx = accumulator.findIndex((triple) => triple.predicate.value == currentTriple.predicate.value);
+    if (existingPredicateNdx >= 0) {
+      if (!accumulator[existingPredicateNdx].subjects) {
         accumulator[existingPredicateNdx].subjects = [accumulator[existingPredicateNdx].subject];
         accumulator[existingPredicateNdx].subject = null;
       }
       accumulator[existingPredicateNdx].subjects.push(currentTriple.subject);
-    }else{
+    } else {
       accumulator.push(currentTriple);
     }
     return accumulator;
   };
 
-  sortByPredicateImportance = (a,b)=> this.predicate_sort_order.indexOf(a.predicate.value) - this.predicate_sort_order.indexOf(b.predicate.value);
+  sortByPredicateImportance = (a, b) => this.predicate_sort_order.indexOf(a.predicate.value) - this.predicate_sort_order.indexOf(b.predicate.value);
 
+  deepCopyTriple = (src) => {
+    return {
+      subject: this.store.sym(src.subject.value),
+      predicate: this.store.sym(src.predicate.value),
+      object: src.object.termType === "Literal"? this.store.literal(src.object.value) : this.store.sym(src.object.value)
+    }
+  };
 
-  onNodeClick(node){
-    this.triples=[];
+  onNodeClick(node) {
+    this.triples = [];
     this.current_node = node;
-    let outgoing_and_owned_triples=this.store.match(node, undefined, undefined);
+    let outgoing_and_owned_triples = this.store.match(node, undefined, undefined);
 
-    this.own_triples = outgoing_and_owned_triples
+    this.own_triples = Array.from(outgoing_and_owned_triples)
       .filter(this.filterTripleWithLiteralObject)
+      .map(this.deepCopyTriple)
       .reduce(this.reducerGroupByPredicate, [])
       .sort(this.sortByPredicateImportance)
     ;
-    this.outgoing_triples = outgoing_and_owned_triples
+    this.outgoing_triples = Array.from(outgoing_and_owned_triples)
       .filter(this.filterNavigableNodes)
+      .map(this.deepCopyTriple)
       .reduce(this.reducerGroupByPredicateForLinkableObjects, [])
       .sort(this.sortByPredicateImportance)
 
     this.incoming_triples = this.store.match(undefined, undefined, node)
+      .map(this.deepCopyTriple)
       .reduce(this.reducerGroupByPredicateForLinkableSubjects, [])
       .sort(this.sortByPredicateImportance);
   }
 
-   getShortValue(node){
-      if (!node || !node.value) {
-        return "";
-      }
-      return node.value.split(/\/|#/).pop().replace( /([A-Z])/g, " $1" ).toLowerCase();
-   }
-
-  replaceDirectionWithArrow(direction){
-    switch(direction){
-      case "in": return "->";
-      case "out": return "<-";
-      case "both": return "<->";
+  getShortValue(node) {
+    if (!node || !node.value) {
+      return "";
     }
+    return node.value.split(/\/|#/).pop().replace(/([A-Z])/g, " $1").toLowerCase();
   }
 
-  getShortValuesAndDirection(links){
-    return links.map((link)=>this.getShortValue(link.predicate) + " " + this.replaceDirectionWithArrow(link.direction)).join(", ");
-  }
+  // replaceDirectionWithArrow(direction){
+  //   switch(direction){
+  //     case "in": return "->";
+  //     case "out": return "<-";
+  //     case "both": return "<->";
+  //   }
+  // }
 
-  getTypeOrEmptyString(node){
-    if (!node){
+  // getShortValuesAndDirection(links){
+  //   return links.map((link)=>this.getShortValue(link.predicate) + " " + this.replaceDirectionWithArrow(link.direction)).join(", ");
+  // }
+
+  getTypeOrEmptyString(node) {
+    if (!node) {
       return ""
     }
     let typeNodes = this.store.match(node, this.store.sym("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"), undefined);
-    return typeNodes.map(node=>this.getShortValue(node.object)).join(", ");
+    return typeNodes.map(node => this.getShortValue(node.object)).join(", ");
 
   }
 
